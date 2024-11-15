c43723385800b8998e0a56d011922cd3ec71cbcc
shivam-gupta-022 <shivam9410667735@gmail.com>
2024-11-15 20:56:52 +0530

    cors updated

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+//new changes
+const cors = require('cors')
 require('dotenv').config()
 global.foodData = require('./db')(function call(err, data, CatData) {
   //  console.log(data)
@@ -6,9 +8,14 @@ global.foodData = require('./db')(function call(err, data, CatData) {
   global.foodData = data;
   global.foodCategory = CatData;
 })
+// new changes
 
 const express = require('express')
 const app = express()
+app.use(cors({
+  origin: 'https://playful-babka-3c6cd3.netlify.app/',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+}))
 const port = process.env.PORT || 4000
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
